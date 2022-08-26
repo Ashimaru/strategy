046104591efdcff08f71042ede937f9a37a8f171
Leoneedus <marmaz.966@o2.pl>
2022-08-26 18:22:19 +0200

    Add map scrolling on drag (mouse middle button) and when the cursor is near the screen edge (disabled in Editor Mode because can be annoying)

---
+++ Strategy/Assets/Code/Scripts/CameraMovementController.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementController : MonoBehaviour
+{
+    private Vector3 origin;
+    private Vector3 difference;
+
+    private readonly Rect screenSpaceBounds = new Rect(Screen.width * 0.01f, Screen.height * 0.01f, Screen.width * 0.98f, Screen.height * 0.98f);
+
+    [SerializeField]
+    private float scrollSpeed = 15;
+
+    void LateUpdate()
+    {
+        if (DragCamera())
+        {
+            return;
+        }
+#if !UNITY_EDITOR
+        ScrollCameraIfCoursorNearTheEdge();
+#endif
+    }
+
+    bool DragCamera()
+    {
+        if (Input.GetMouseButtonDown(2))
+        {
+            origin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            return true;
+        }
+
+        if (Input.GetMouseButton(2))
+        {
+            difference = (Camera.main.ScreenToWorldPoint(Input.mousePosition)) - Camera.main.transform.position;
+            Camera.main.transform.position = origin - difference;
+            return true;
+        }
+
+        return false;
+    }
+
+    void ScrollCameraIfCoursorNearTheEdge()
+    {
+        var pos = Input.mousePosition;
+        var screenSpacePosition = new Vector2(pos.x, pos.y);
+        if (!screenSpaceBounds.Contains(screenSpacePosition))
+        {
+            var direction = new Vector3(screenSpacePosition.x, screenSpacePosition.y) - new Vector3(screenSpaceBounds.center.x, screenSpaceBounds.center.y);
+            direction.Normalize();
+            Camera.main.transform.position += direction * Time.deltaTime * scrollSpeed;
+        }
+    }
+}
